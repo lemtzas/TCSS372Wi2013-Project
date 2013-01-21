ad52fa26af0bdac956920ea818be747dda4bd4e5
Lemtzas <lemtzas@gmail.com>
2013-01-21 13:13:44 -0800

    Initial Commit.

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Lemtzas
+ *
+ * Created on January 21, 2013, 1:12 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello");
+    return (EXIT_SUCCESS);
+}
+
