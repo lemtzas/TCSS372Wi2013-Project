b95568ab51ae18e27cd91d3ba711eec5ec71e438
Lemtzas <lemtzas@gmail.com>
2013-01-26 21:03:55 -0800

    Fixed ALU errors.

--- register.c
+++ register.c
@@ -1,1 +1,26 @@
 #include "register.h"
+
+char* reg_init(Register *this)
+{
+    return 0;
+}
+/**Set Low Order Byte.*/
+char* reg_setLOB(Register* this, Byte b)
+{
+    return 0;
+}
+/**Set High Order Byte.*/
+char* reg_setHOB(Register* this, Byte b)
+{
+    return 0;
+}
+/**Set Low Order Byte.*/
+char* reg_getLOB(Register* this, Byte* b)
+{
+    return 0;
+}
+/**Set High Order Byte.*/
+char* reg_getHOB(Register* this, Byte* b)
+{
+    return 0;
+}
