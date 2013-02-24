2ef3035882c58a376d19d9b8b7725778602ae9d1
Lemtzas <lemtzas@gmail.com>
2013-02-24 00:33:38 -0800

    Defined StatusWord for convenience.

--- global.h
+++ global.h
@@ -8,13 +8,27 @@
 #ifndef GLOBAL_H
 #define	GLOBAL_H
 
+#include <stdint.h>
+
 typedef uint16_t Word;
 typedef unsigned char Byte;
 typedef unsigned char Boolean;
+
+typedef union {
+    struct {
+        unsigned char irq_lines:8;
+        unsigned char reserved:4;
+        unsigned char O:1;
+        unsigned char C:1;
+        unsigned char Z:1;
+        unsigned char N:1;
+    }fields;
+    Word full_data;
+} StatusWord;
+
+
 #define FALSE 0
 #define TRUE  1
-
-#include <stdint.h>
 
 
 #endif	/* GLOBAL_H */
