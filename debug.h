97b116ab2b4f8d73fef7a67713bbb1881b8ce559
Lemtzas <lemtzas@gmail.com>
2013-02-24 10:31:20 -0800

    Debug screen action setup.

--- debug.h
+++ debug.h
@@ -12,7 +12,20 @@
 #include "register.h"
 #include "memory.h"
 
+#define COMMAND_INVALID -1
+#define COMMAND_QUIT    0
+#define COMMAND_LOAD    1
+#define COMMAND_RUN     2
+#define COMMAND_STEP    3
+#define COMMAND_DUMP    4
+
+#include "instFile.h"
+
 char* debug_entry(CPU *cpu,Memory *memory);
+char* debug_entry_LOAD(CPU *cpu,Memory *memory);
+char* debug_entry_RUN(CPU *cpu,Memory *memory);
+char* debug_entry_STEP(CPU *cpu,Memory *memory);
+char* debug_entry_DUMP(CPU *cpu,Memory *memory);
 
 
 #endif	/* DEBUG_H */
