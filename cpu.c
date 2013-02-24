e7103b06facb5b92b4435c520c78434b5f182acc
Lemtzas <lemtzas@gmail.com>
2013-02-23 23:42:39 -0800

    Push/Pop. Syntax Fixes.

--- cpu.c
+++ cpu.c
@@ -2,6 +2,7 @@
 
 char* cpu_init(CPU *cpu) {
     cpu->halt = 0;
+    cpu->rf.registers[REG_SP] = SP_START;
     return 0;
 }
 
@@ -16,7 +17,7 @@ char* cpu_getbyte(CPU *cpu) {
 }
 
 char* cpu_setword(CPU *cpu) {
-    return mem_getw(&(cpu->memory) , cpu->MAR , cpu->MDR);
+    return mem_setw(&(cpu->memory) , cpu->MAR , cpu->MDR);
 }
 
 char* cpu_setbyte(CPU *cpu) {
@@ -34,8 +35,8 @@ char* cpu_inst_fetch(CPU *cpu) {
 
 //return 0-3
 char* cpu_inst_decode(CPU *cpu) {
-    cpu_instruction = (cpu_instruction)(cpu->IR);
-    switch(cpu_instruction.opcode.parts.op1) {
+    cpu_instruction* ir = (cpu_instruction*)(&(cpu->IR));
+    switch(ir->opcode.parts.op1) {
         case OP1_LDST:
             cpu_inst_LDST(cpu);
             break;
@@ -50,8 +51,8 @@ char* cpu_inst_decode(CPU *cpu) {
 
 //return 0-3
 char* cpu_inst_LDST(CPU *cpu) {
-    cpu_instruction = (cpu_instruction)(cpu->IR);
-    switch(cpu_instruction.opcode.parts.op2) {
+    cpu_instruction* ir = (cpu_instruction*)(&(cpu->IR));
+    switch(ir->opcode.parts.op2) {
         case OP2_LDI:
             return cpu_inst_LDST_LDI(cpu);
             break;
@@ -70,58 +71,61 @@ char* cpu_inst_LDST(CPU *cpu) {
         case OP2_STW:
             return cpu_inst_LDST_STW(cpu);
             break;
+        case OP2_STACK:
+            return cpu_inst_LDST_STACK(cpu);
+            break;
         default:
-            return "not implemented";
+            return "opcode does not exist";
     }
     return 0;
 }
 
 char* cpu_inst_LDST_LDI(CPU *cpu) {
-    cpu_inst_format1 ir = (cpu_inst_format1)(cpu->IR);
-    cpu->rf.registers [ir->d] = ir.immed7;
+    cpu_inst_format1* ir = (cpu_inst_format1*)(&(cpu->IR));
+    cpu->rf.registers [ir->d] = ir->immed7;
     return 0;
 }
 char* cpu_inst_LDST_LDA(CPU *cpu) {
-    cpu_inst_format0 ir = (cpu_inst_format0)(cpu->IR);
+    cpu_inst_format0* ir = (cpu_inst_format0*)(&(cpu->IR));
     //load and step
     cpu->MAR = cpu->PC;
     cpu->PC += 2;
     //read mem
     char* err = cpu_getword(cpu);
     //store
-    cpu->rf.registers[ir.word1.d] = cpu->MDR;
+    cpu->rf.registers[ir->word1.d] = cpu->MDR;
     return err;
 }
 char* cpu_inst_LDST_LDB(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     char* err;
-    switch(ir.mod) {
+    switch(ir->mod) {
         case MOD_MODE_REGISTER:
              // $Rd(lob) ← M[$Ra]  register mode
-            cpu->MAR = cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[ir->a];
             err = cpu_getbyte(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_RELATIVE:
             // $Rd(lob) ← M[BP + $Ra]  base-relative
-            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir->a];
             err = cpu_getbyte(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_INDEX:
             // $Rd(lob) ← M[IP-0]; IP-0 ← IP-0 + 1    index, register 0xC
-            cpu->MAR = cpu->rf.registers[REG_IP_0] + cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[REG_IP_0] + cpu->rf.registers[ir->a];
             cpu->rf.registers[REG_IP_0]++;
             err = cpu_getbyte(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_INDIRECT:
             // $Rd(lob) ← M[M[$Ra]]    indirect mode
-            cpu->MAR = cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[ir->a];
             err = cpu_getword(cpu); if(err) return err;
             cpu->MAR = cpu->MDR;
             err = cpu_getbyte(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         default:
             return "Undefined operation";
@@ -130,46 +134,46 @@ char* cpu_inst_LDST_LDB(CPU *cpu) {
     return 0;
 }
 char* cpu_inst_LDST_LDWMOV(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     char* err;
-    switch(ir.mod) {
+    switch(ir->mod) {
         case MOD_MODE_REGISTER:
             // $Rd(hob) ← M[$Ra]; $Rd(lob) ← M[$Ra + 1]
-            cpu->rf.registers[ir.d] = 0;
-            cpu->MAR = cpu->rf.registers[ir.a];
+            cpu->rf.registers[ir->d] = 0;
+            cpu->MAR = cpu->rf.registers[ir->a];
             err = cpu_getword(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_RELATIVE:
             // $Rd(hob) ← M[BP + $Ra];
             // $Rd(lob) ← M[BP + $Ra + 1]  base-relative
-            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir->a];
             err = cpu_getword(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_INDEX:
             // $Rd(hob) ← M[IP-0]; IP-0 ← IP-0 + 1;
             // $Rd(lob) ← M[IP-0]; IP-0 ← IP-0 + 1
             // index, register 0xC
-            cpu->MAR = cpu->rf.registers[REG_IP_0] + cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[REG_IP_0] + cpu->rf.registers[ir->a];
             cpu->rf.registers[REG_IP_0] += 2;
             err = cpu_getword(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_INDIRECT:
             // $Rd(hob) ← M[M[$Ra]];
             // $Rd(lob) ← M[M[$Ra + 1]]
             // indirect mode
-            cpu->MAR = cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[ir->a];
             err = cpu_getword(cpu); if(err) return err;
             cpu->MAR = cpu->MDR;
             err = cpu_getword(cpu); if(err) return err;
-            cpu->rf.registers[ir.d] = cpu->MDR;
+            cpu->rf.registers[ir->d] = cpu->MDR;
             break;
         case MOD_MODE_MOV:
             // $Rd ← ($Rs)
             // register to register move
-            cpu->rf.registers[ir.d] = cpu->rf.registers[ir.a];
+            cpu->rf.registers[ir->d] = cpu->rf.registers[ir->a];
             break;
         default:
             return "Undefined operation";
@@ -178,36 +182,36 @@ char* cpu_inst_LDST_LDWMOV(CPU *cpu) {
     return 0;
 }
 char* cpu_inst_LDST_STB(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     char* err;
-    switch(ir.mod) {
+    switch(ir->mod) {
         case MOD_MODE_REGISTER:
              // M[$Ra] ← ($Rs(lob))
-            cpu->MAR = cpu->rf.registers[ir.a];
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MAR = cpu->rf.registers[ir->a];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setbyte(cpu); if(err) return err;
             break;
         case MOD_MODE_RELATIVE:
             // M[BP + $Ra] ← ($Rs(lob))
-            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir.a];
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir->a];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setbyte(cpu); if(err) return err;
             break;
         case MOD_MODE_INDEX:
             // M[IP-1] ← ($Rs(lob)); IP-1 ← IP-1 + 1
             // index mode, register 0xD
-            cpu->MAR = cpu->rf.registers[REG_IP_1] + cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[REG_IP_1] + cpu->rf.registers[ir->a];
             cpu->rf.registers[REG_IP_1]++;
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setbyte(cpu); if(err) return err;
             break;
         case MOD_MODE_INDIRECT:
             // M[M[$Ra]] ← ($Rs(lob))
             // indirect mode
-            cpu->MAR = cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[ir->a];
             err = cpu_getword(cpu); if(err) return err;
             cpu->MAR = cpu->MDR;
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setbyte(cpu); if(err) return err;
             break;
         default:
@@ -217,39 +221,39 @@ char* cpu_inst_LDST_STB(CPU *cpu) {
     return 0;
 }
 char* cpu_inst_LDST_STW(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     char* err;
-    switch(ir.mod) {
+    switch(ir->mod) {
         case MOD_MODE_REGISTER:
             // M[$Ra] ← ($Rs(hob)); M[$Ra + 1] ← ($Rs(lob))
-            cpu->MAR = cpu->rf.registers[ir.a];
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MAR = cpu->rf.registers[ir->a];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setword(cpu); if(err) return err;
             break;
         case MOD_MODE_RELATIVE:
             // M[BP + $Ra] ← ($Rs(hob));
             // M[BP + $Ra + 1] ← ($Rs(lob))
-            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir.a];
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MAR = cpu->rf.registers[REG_BP] + cpu->rf.registers[ir->a];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setword(cpu); if(err) return err;
             break;
         case MOD_MODE_INDEX:
             // M[IP-1] ← ($Rs(hob)); IP-1 ← IP-1 + 1;
             // M[IP-1] ← ($Rs(lob)); IP-1 ← IP-1 + 1
             // index mode, register 0xD
-            cpu->MAR = cpu->rf.registers[REG_IP_1] + cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[REG_IP_1] + cpu->rf.registers[ir->a];
             cpu->rf.registers[REG_IP_1]++;
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setword(cpu); if(err) return err;
             break;
         case MOD_MODE_INDIRECT:
             // M[M[$Ra]] ← ($Rs(hob));
             // M[M[$Ra + 1]] ← ($Rs(lob))
             // indirect mode
-            cpu->MAR = cpu->rf.registers[ir.a];
+            cpu->MAR = cpu->rf.registers[ir->a];
             err = cpu_getword(cpu); if(err) return err;
             cpu->MAR = cpu->MDR;
-            cpu->MDR = cpu->rf.registers[ir.d];
+            cpu->MDR = cpu->rf.registers[ir->d];
             err = cpu_setword(cpu); if(err) return err;
             break;
         default:
@@ -258,10 +262,63 @@ char* cpu_inst_LDST_STW(CPU *cpu) {
     
     return 0;
 }
+char* cpu_inst_LDST_STACK(CPU *cpu) {
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
+    switch(ir->mod) {
+        case MOD_STACK_PUSHB:
+            // SP ← SP - 1; M[SP] ← ($Rs)
+            //TOS points to last value pushed
+            // 0123456789
+            //          ^
+            //         ^ 
+            //         b
+            cpu->rf.registers[REG_SP]--;
+            cpu->MAR = cpu->rf.registers[REG_SP];
+            cpu->MDR = cpu->rf.registers[ir->d];
+            cpu_setbyte(cpu);
+            break;
+        case MOD_STACK_PUSHW:
+            // SP ← SP - 1; M[SP] ← ($Rs)(lob);
+            // SP ← SP - 1; M[SP] ← ($Rs)(hob)
+            // 0123456789
+            //          ^
+            //        ^
+            //        hl 
+            cpu->rf.registers[REG_SP]-=2;
+            cpu->MAR = cpu->rf.registers[REG_SP];
+            cpu->MDR = cpu->rf.registers[ir->d];
+            cpu_setword(cpu);
+            break;
+        case MOD_STACK_POPB:
+            // $Rd(lob) ← M[SP]; SP ← SP + 1 NEED FOCUS. BAAAAAH.
+            // 0123456789
+            //         ^
+            //         b^
+            cpu->MAR = cpu->rf.registers[REG_SP];
+            cpu->rf.registers[REG_SP]++;
+            cpu_getbyte(cpu);
+            cpu->rf.registers[ir->d] = cpu->MDR;
+            break;
+        case MOD_STACK_POPW:
+            // $Rd(hob) ← M[SP]; SP ← SP + 1;
+            // $Rd(lob) ← M[SP]; SP ← SP + 1
+            // 0123456789
+            //        ^
+            //        hl^
+            cpu->MAR = cpu->rf.registers[REG_SP];
+            cpu->rf.registers[REG_SP]+=2;
+            cpu_getword(cpu);
+            cpu->rf.registers[ir->d] = cpu->MDR;
+            break;
+        default:
+            return "invalid value";
+    }
+    return 0;
+}
 
 char* cpu_inst_ALU(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
-    switch(ir.opcode.parts.op2) {
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
+    switch(ir->opcode.parts.op2) {
         case OP2_OPER:
             return cpu_inst_ALU_OPER(cpu);
             break;
@@ -276,13 +333,13 @@ char* cpu_inst_ALU(CPU *cpu) {
 }
 
 char* cpu_inst_ALU_OPER(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     char* err = 0;
     ALU* alu = &(cpu->alu);
     //set x and y
-    err = alu_set_rs(alu,cpu->rf.registers[ir.d],cpu->rf.registers[ir.a]);
+    err = alu_set_rs(alu,cpu->rf.registers[ir->d],cpu->rf.registers[ir->a]);
     if(err) return err;
-    switch(ir.mod) {
+    switch(ir->mod) {
         case MOD_OPER_ADD:
             err = alu_op_add(alu); if(err) return err;
             break;
@@ -315,12 +372,12 @@ char* cpu_inst_ALU_OPER(CPU *cpu) {
 }
 
 char* cpu_inst_ALU_SHLR(CPU *cpu) {
-    cpu_inst_format2 ir = (cpu_inst_format2)(cpu->IR);
+    cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     char* err = 0;
     ALU* alu = &(cpu->alu);
     //set x and y
-    err = alu_set_rx(alu,cpu->rf.registers[ir.d]);
-    switch(ir.opcode.parts.op2) {
+    err = alu_set_rx(alu,cpu->rf.registers[ir->d]);
+    switch(ir->opcode.parts.op2) {
         case OP2_SHL:
             err = alu_op_shl(alu); if(err) return err;
             break;
