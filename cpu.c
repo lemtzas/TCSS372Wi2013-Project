62e73f5a7036b64e0f67f044cc4ec43108a40ebc
Lemtzas <lemtzas@gmail.com>
2013-02-25 22:38:04 -0800

    Commenting; cpu init initializes memory now

--- cpu.c
+++ cpu.c
@@ -11,6 +11,8 @@ char* cpu_init(CPU *cpu) {
     cpu->MDR = MDR_START;
     cpu->OSB = OSB_START;
     cpu->TXB = TXB_START;
+    char* err = mem_init(&(cpu->memory));
+    if(err) return err;
     return alu_init(&(cpu->alu), &(cpu->SW));
 }
 
@@ -64,7 +66,7 @@ char* cpu_inst_decode(CPU *cpu) {
 }
 
 
-//return 0-3
+//various LD and ST instructions
 char* cpu_inst_LDST(CPU *cpu) {
     cpu_instruction* ir = (cpu_instruction*)(&(cpu->IR));
     switch(ir->op2) {
@@ -330,6 +332,7 @@ char* cpu_inst_LDST_STACK(CPU *cpu) {
     return 0;
 }
 
+//various ALU instructions
 char* cpu_inst_ALU(CPU *cpu) {
     cpu_inst_format2* ir = (cpu_inst_format2*)(&(cpu->IR));
     switch(ir->op2) {
@@ -401,6 +404,7 @@ char* cpu_inst_ALU_SHLR(CPU *cpu) {
     return err;
 }
 
+//various CONTROL instructions
 char* cpu_inst_CONTROL(CPU *cpu) {
     cpu_instruction* ir = (cpu_instruction*)(&(cpu->IR));
     switch(ir->op2) {
@@ -545,6 +549,7 @@ char* cpu_inst_CONTROL_RET(CPU *cpu){
     return 0;
 }
 
+//supervisor and misc instruction base (implements HALT and NOP)
 char* cpu_inst_SUPMISC(CPU *cpu) {
     cpu_instruction* ir = (cpu_instruction*)(&(cpu->IR));
     switch(ir->op2) {
@@ -589,17 +594,18 @@ char* cpu_step(CPU *cpu) {
 //    return 0;
 //}
 //step the "controller" until halt or instruction limit
-char* cpu_run_limited(CPU *cpu, int limit) {
-    char* err_buff;
-    while(!(cpu->halt) && (limit--) > 0) {
-        err_buff = cpu_step(cpu);
-        if(err_buff)
-            return err_buff;
-    }
-    if(limit == 0)
-        return "Limit Reached";
-    return 0;
-}
+
+//char* cpu_run_limited(CPU *cpu, int limit) {
+//    char* err_buff;
+//    while(!(cpu->halt) && (limit--) > 0) {
+//        err_buff = cpu_step(cpu);
+//        if(err_buff)
+//            return err_buff;
+//    }
+//    if(limit == 0)
+//        return "Limit Reached";
+//    return 0;
+//}
 
 
 uint16_t sext(Word word, int bits) {
